7e475dcc96a4d796dfbdac298d86d876771f102c
root <root@beaglebone.(none)>
2014-04-23 20:52:03 +0000

    2 channel ADC to get door sensor data

--- BESI_C/Demo/Demo_ADC/ADC_try1.c
+++ BESI_C/Demo/Demo_ADC/ADC_try1.c
@@ -4,7 +4,6 @@ measures audio at 10KHz with Mic connected to AIN1 pin on BBB
 output is the sum of the differences between each sample and 
 OFFSET over SAMPLE_WINDOW
 Usage: ./ADC <Start Time>
-
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,28 +13,35 @@ Usage: ./ADC <Start Time>
 #define MIC_BUFFER_SIZE 10000
 #define PIR_BUFFER_SIZE 10000
 #define TEMP_BUFFER_SIZE 10
+
 #define MIC_SAMPLE_SIZE 10000
 #define PIR_SAMPLE_SIZE 10000
 #define TEMP_SAMPLE_SIZE 10
-#define SAMPLE_WINDOW 100
+
 #define MIC_OFFSET 0.9
-#define PIR_OFFSET 0
+#define PIR1_OFFSET 0
+#define PIR2_OFFSET 0
+
+#define SAMPLE_SIZE (MIC_SAMPLE_SIZE>PIR_SAMPLE_SIZE?(int)MIC_SAMPLE_SIZE:(int)PIR_SAMPLE_SIZE)
+#define SAMPLE_WINDOW 100
 #define TIMESTEP (float)SAMPLE_WINDOW/10000
+
 /* ----------------------------------------------------------- */
 float highPassFilter(float last_output, float input, float last_input);
 
 int main(int argc, const char* argv[])
 {
-	unsigned int mic_sample,pir_sample,temp_sample;
+	unsigned int mic_sample,pir1_sample,pir2_sample,temp_sample;
 	int i ,j, k;
 	unsigned int buffer_AIN_1[MIC_BUFFER_SIZE] = {0};
 	unsigned int buffer_AIN_2[PIR_BUFFER_SIZE] = {0};
+	unsigned int buffer_AIN_3[PIR_BUFFER_SIZE] = {0};
 	unsigned int buffer_AIN_0[TEMP_BUFFER_SIZE] = {0};
 	FILE *adc_file;
 	FILE *temp_file;
 	struct timeval t_start, t_end;
  	float mTime =0;
- 	float mic_avg, pir_avg, mic_out, last_mic_sample, last_mic_output, filter_out;
+ 	float mic_avg, pir1_avg, pir2_avg, mic_out, last_mic_sample, last_mic_output, filter_out;
 
 	/* BBBIOlib init*/
 	iolib_init();
@@ -49,7 +55,7 @@ int main(int argc, const char* argv[])
 	 *
 	 */
 //	const int clk_div = 34 ;
-	const int clk_div = 80;
+	const int clk_div = 53;
 	const int open_dly = 0;
 	const int sample_dly = 1;
 	
@@ -65,24 +71,27 @@ int main(int argc, const char* argv[])
 	 */
 	BBBIO_ADCTSC_module_ctrl(BBBIO_ADC_WORK_MODE_TIMER_INT, clk_div);
 
-	BBBIO_ADCTSC_channel_ctrl(BBBIO_ADC_AIN1, BBBIO_ADC_STEP_MODE_SW_CONTINUOUS, open_dly, sample_dly, \
-				BBBIO_ADC_STEP_AVG_1, buffer_AIN_1, MIC_BUFFER_SIZE);
-	BBBIO_ADCTSC_channel_ctrl(BBBIO_ADC_AIN2, BBBIO_ADC_STEP_MODE_SW_CONTINUOUS, open_dly, sample_dly, \
-				BBBIO_ADC_STEP_AVG_1, buffer_AIN_2, PIR_BUFFER_SIZE);
-	BBBIO_ADCTSC_channel_ctrl(BBBIO_ADC_AIN0, BBBIO_ADC_STEP_MODE_SW_CONTINUOUS, open_dly, sample_dly, \
-				BBBIO_ADC_STEP_AVG_1, buffer_AIN_0, TEMP_BUFFER_SIZE);
+	BBBIO_ADCTSC_channel_ctrl(BBBIO_ADC_AIN1, BBBIO_ADC_STEP_MODE_SW_CONTINUOUS,\
+		open_dly, sample_dly, BBBIO_ADC_STEP_AVG_1, buffer_AIN_1, MIC_BUFFER_SIZE);
+	BBBIO_ADCTSC_channel_ctrl(BBBIO_ADC_AIN2, BBBIO_ADC_STEP_MODE_SW_CONTINUOUS,\
+		open_dly, sample_dly, BBBIO_ADC_STEP_AVG_1, buffer_AIN_2, PIR_BUFFER_SIZE);
+	BBBIO_ADCTSC_channel_ctrl(BBBIO_ADC_AIN3, BBBIO_ADC_STEP_MODE_SW_CONTINUOUS,\
+		open_dly, sample_dly, BBBIO_ADC_STEP_AVG_1, buffer_AIN_3, PIR_BUFFER_SIZE);
+	BBBIO_ADCTSC_channel_ctrl(BBBIO_ADC_AIN0, BBBIO_ADC_STEP_MODE_SW_CONTINUOUS,\
+		open_dly, sample_dly, BBBIO_ADC_STEP_AVG_1, buffer_AIN_0, TEMP_BUFFER_SIZE);
 	
 	gettimeofday(&t_start, NULL);
 
-        
         BBBIO_ADCTSC_channel_enable(BBBIO_ADC_AIN1);
 	BBBIO_ADCTSC_channel_enable(BBBIO_ADC_AIN2);
+	BBBIO_ADCTSC_channel_enable(BBBIO_ADC_AIN3);
 	BBBIO_ADCTSC_channel_disable(BBBIO_ADC_AIN0);
 	BBBIO_ADCTSC_work(MIC_SAMPLE_SIZE);
 
 	i = 0; k = 0;
-	mic_avg = 0; pir_avg = 0; mic_out = 0; last_mic_sample = 0; last_mic_output = 0;
-	for(j = 0 ; j < MIC_SAMPLE_SIZE ; j++) {
+	mic_avg = 0; mic_out = 0; last_mic_sample = 0; last_mic_output = 0;
+	pir1_avg = 0; pir2_avg = 0;
+	for(j = 0 ; j < (int)SAMPLE_SIZE ; j++) {
 		mic_sample = buffer_AIN_1[j];
 		mic_out = (MIC_OFFSET - ((float)mic_sample / 4095.0f) * 1.8f);
 		filter_out = highPassFilter(last_mic_output, mic_out, last_mic_sample); 
@@ -90,8 +99,10 @@ int main(int argc, const char* argv[])
 		last_mic_sample = mic_out;
 		last_mic_output = filter_out;
 		if (((j+1) % 10) == 0){
-			pir_sample = buffer_AIN_2[j];
-			pir_avg += fabs(PIR_OFFSET - ((float)pir_sample / 4095.0f) * 1.8f);
+			pir1_sample = buffer_AIN_2[j];
+			pir1_avg += fabs(PIR1_OFFSET - ((float)pir1_sample / 4095.0f) * 1.8f);
+			pir2_sample = buffer_AIN_3[j];
+			pir2_avg += fabs(PIR2_OFFSET - ((float)pir2_sample / 4095.0f) * 1.8f);
 			k++;
 		}	
 		if (((j + 1) % SAMPLE_WINDOW) == 0){
@@ -100,8 +111,8 @@ int main(int argc, const char* argv[])
 			mic_avg = 0;
 			if ((k % SAMPLE_WINDOW) == 0){
 				//fprintf(adc_file, "%0.3f,%0.2f\n", mTime + TIMESTEP * (float)i, avg);
-				printf("%0.3f,%0.2f,", mTime + TIMESTEP * (float)i, pir_avg);
-				pir_avg = 0;
+				printf("%0.3f,%0.2f,%0.3f,%0.2f,", mTime + TIMESTEP * (float)i, pir1_avg,  mTime + TIMESTEP * (float)i, pir2_avg);
+				pir1_avg = 0; pir2_avg = 0;
 			}
 			else{
 				printf("");
@@ -112,6 +123,7 @@ int main(int argc, const char* argv[])
 
         BBBIO_ADCTSC_channel_disable(BBBIO_ADC_AIN1);
     	BBBIO_ADCTSC_channel_disable(BBBIO_ADC_AIN2);
+    	BBBIO_ADCTSC_channel_disable(BBBIO_ADC_AIN3);
 
 
 	BBBIO_ADCTSC_channel_enable(BBBIO_ADC_AIN0);
@@ -124,10 +136,9 @@ int main(int argc, const char* argv[])
 	//temp_file = fopen("temp.txt", "a");
 	//printf("temp: %0.3f\n", ((float)sample / 4095.0f) * 1.8f);
 	//fprintf(temp_file, "%0.3f,%0.4f\n", mTime, ((float)sample / 4095.0f) * 1.8f);
-	printf("%0.3f,%0.4f,", mTime, ((float)temp_sample / 4095.0f) * 1.8f);
+	printf("%0.3f,%0.4f", mTime, ((float)temp_sample / 4095.0f) * 1.8f);
 	//fclose(temp_file);
 	BBBIO_ADCTSC_channel_disable(BBBIO_ADC_AIN0);
-
 
 	iolib_free();
 	//fclose(adc_file);
